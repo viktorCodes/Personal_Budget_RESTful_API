165ba5b9dee0068503e74334e0a34034c0e148b5
calimendoza <mbaivictor1@gmail.com>
2022-11-30 23:57:47 -0800

    retrieving all envelopes

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ const envelopesRouter = express.Router();
 const app = express();
 const PORT = process.env.PORT || 4001;
 
+
+const {    
+    Envelopes,
+    getAll,
+    setEnvelope,
+    getEnvelope,
+    deleteEnvelope,
+    update,
+} = require('./envelopes');
+
+
 app.use(bodyParser.json());
 app.use(errorHandler());
 app.use(cors());
@@ -42,3 +53,75 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+// GET
+// Get all
+envelopesRouter.get('/', (req, res, next)=>{
+    res.status(200).send(getAll())
+    console.log('\n*****Getting all envelopes')
+})
+// Get individual
+envelopesRouter.get('/:category', (req, res, next) => {
+    res.status(200).send(req.envelope);
+    console.log('\n*****Got envelope - ' + req.category + ': ' + (req.envelope[req.category]));
+});
+
+// PUT 
+envelopesRouter.put('/:category/:action/', (req,res,next)=>{
+    let category = req.category;
+    let action = req.action;
+    let amount = req.query.amount;
+    let updated = update(action, category, amount);
+    if (updated) {
+        res.send(updated);
+        console.log(`\n*****Envelope updated - ${action} ${req.category}: ${req.query.amount}`)
+    } else {
+        res.status(404).send('something went wrong, Chief...')
+    }
+})
+
+// POST
+envelopesRouter.post('/', (req,res,next)=>{
+    // let category = req.body.envelope;
+    // let amount = req.body.amount;
+    let category = req.body['envelope'];
+    let amount = req.body['amount'];    
+    let newEnvelope = setEnvelope(category, amount);
+    if (newEnvelope) {
+        res.status(201).send(newEnvelope)
+        console.log('\n*****Created the envelope - ' + category + ': ' + amount);
+    } else {
+        res.status(404).send();
+    }
+})
+// TRANSFER
+envelopesRouter.post('/transfer/:from/:to', (req,res,next)=>{
+    let from = req.params.from;
+    let to = req.params.to;
+    let amount = req.query.amount;
+    let updatedFrom = update('debit', from, amount);
+    let updatedTo = update('credit', to, amount);
+    let returnArr = [updatedFrom, updatedTo]
+    console.log(`\n*****Transfered ${amount} from ${from} to ${to}`);
+    res.status(200).send(returnArr)
+})
+
+// DELETE 
+envelopesRouter.delete('/:category', (req,res,next)=>{
+    const category = req.category;
+    const deleted = deleteEnvelope(category);
+    if (deleted) {
+        res.status(204)
+        console.log('\n*****Deleted envelope: ' + category)
+    } else {
+        res.status(500);
+    }
+    res.send();    
+})
+
+// TEST CALLS
+// setEnvelope('rent', 1000);
+// setEnvelope('electric', 50);
+// setEnvelope('internet', 60);
+// setEnvelope('groceries', 300);
+// setEnvelope('gasoline', 100);
--- src/routes/envelopes.js
+++ src/routes/envelopes.js
@@ -1,95 +1,54 @@
-const express = require('express');
-const envelopesRouter = express.Router();
-const Database = require('../database/database');
-const dbPath = "./src/database/database.json";
-const db = new Database(dbPath);
+let Envelopes = {};
 
-envelopesRouter.param('id', (req, res, next, id) => {
-    if(id){
-        const envelope = db.getEnvelope(Number(id));
-        if(envelope){
-            req.envelope = envelope;
-            next();
-        }else{
-            res.status(404).send('Invalid Id.');
-        }
-    }else {
-        req.envelope = null;
-        next();
-    }
-    
-});
+/* Helper Functions */
+// Function to get all Envelopes 
+const getAll = () => {
+    return Envelopes;
+}
+// Function to set a budget amount;
+const setEnvelope = (category, amount=0) => {
+    let returnObj = {};
+    Envelopes[category] = amount;
+    return {[category]: amount};
+};
+// Function to get an envelope balance;
+const getEnvelope = category => {
+    if (typeof category!=='string' || !Envelopes[category]) return false;
+    let returnObj = {}
+    returnObj[category] = Envelopes[category];
+    return returnObj;
+}
+// Function to delete an envelope;
+const deleteEnvelope = category => {
+    if (typeof category!=='string') return false;
+    delete Envelopes[category];
+    return true;
+}
 
-envelopesRouter.post('/', (req, res, next) => {
-    const label = req.body.label;
-    const value = req.body.value;
-    if(label && value){
-        if((typeof value !== 'number') || (value < 0)){
-            res.status(400).send('Value must be a number bigger than 0');
-        }else{
-            const newEnvelope = db.addEnvelope(label, value);
-            res.status(201).send(newEnvelope);
-        }
-    }else{
-        res.status(400).send('Missing Data');
-    }
-});
+// Function to debit/credit an envelope by an amount
+const update = (action, category, amount) => {
+    let current = getEnvelope(category)[category];
+    if (action==='credit') return setEnvelope(category, (current+Number(amount)));
+    else if (action==='debit') return setEnvelope(category, (current-Number(amount))); 
+}
 
-envelopesRouter.post('/update/:id', (req, res, next) => {
-    const newValue = req.body.value;
-    if(typeof newValue === "number"){
-        try{
-            const updatedEnvelope = db.updateEnvelope(req.envelope.id, newValue);
-            res.status(200).send(updatedEnvelope);
-        }catch(err){
-            res.status(500).send(err);
-        }
-    }else{
-        res.status(400).send('Invalid value.');
-    }
-});
+/* EXPORTS */
+module.exports = {
+    Envelopes,
+    getAll,
+    setEnvelope,
+    getEnvelope,
+    deleteEnvelope,
+    update
+};
 
-envelopesRouter.post('/transfer/:from/:to',(req, res, next) => {
-    const fromEnvelope = db.getEnvelope(Number(req.params.from));
-    const toEnvelope = db.getEnvelope(Number(req.params.to));
-    const value = req.body.value;
-    if(fromEnvelope && toEnvelope){
-        if(fromEnvelope.value - value <= 0){
-            res.status(403).send('Insufficient funds');
-        }else{
-            const newFromValue = fromEnvelope.value - value;
-            const newToValue = toEnvelope.value + value;
-            try{
-                const updatedFromEnvelope = db.updateEnvelope(fromEnvelope.id, newFromValue);
-                const updatedToEnvelope = db.updateEnvelope(toEnvelope.id, newToValue);
-                const data = {
-                    "from": updatedFromEnvelope,
-                    "to": updatedToEnvelope}
-                res.status(200).send(data);
-            }catch(err){
-                res.status(500).send(err);
-            }
-        }
-    }else{
-        res.status(400).send('Invalid envelop.');
-    }
-});
-
-envelopesRouter.get('/', (req, res, next) => {
-    res.status(200).send(db.getEnvelope());
-});
-
-envelopesRouter.get('/:id', (req, res, next) => {
-    res.status(200).send(req.envelope);
-});
-
-envelopesRouter.delete('/:id', (req, res, next) => {
-    const removedEnvelope = db.removeEnvelope(req.envelope.id);
-    if(removedEnvelope){
-        res.status(200).send(removedEnvelope);
-    } else{
-        res.status(500).send('Something went wrong.');
-    }
-});
-
-module.exports = envelopesRouter;
+// SAMPLE ENVELOPES - comment or uncomment this section
+// setEnvelope('rent', 500)
+// setEnvelope('gas', 50)
+// setEnvelope('electric', 40)
+// setEnvelope('internet', 60)
+// setEnvelope('phone', 55)
+// setEnvelope('water', 35)
+// setEnvelope('cable', 85)
+// setEnvelope('groceries', 355)
+// setEnvelope('beer', 55)
